cd0f045ab47b25570fc53c4f09bf189a9ea9bfb2
Mount-Blanc <mountblanc100@gmail.com>
2023-06-11 13:34:31 -0700

    Added list of operations

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,19 @@ public class Main {
         System.out.print("Enter the first number: ");
         double num1 = scanner.nextDouble();
 
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Select an operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.print("Enter your choice: ");
+        int choice = scanner.nextInt();
 
 
 
-        }
+
+    }
     }
